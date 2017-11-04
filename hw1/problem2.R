75158d0ccab4eb0b48451c7e1a886df331b7718d
moonRockTrader <32208323+moonRockTrader@users.noreply.github.com>
2017-11-03 21:38:26 -0400

    start commit of homework 1 problem 2
    initial commit

---
+++ hw1/problem2.R
@@ -0,0 +1,28 @@
+beta = 0.5
+b = 2
+sigma = 2
+a = 1.1
+
+#Estimation functions
+#Estimation using the conditional expectation of Y|X
+f_condexp = function(x){beta*x}
+
+#TODO: Put your function in here.  You can reference a,b,sigma, and it will just pull them from
+# the outside namespace
+f_yours = function(x){FILL_ME_IN}
+
+#Define the loss function, where z = y - yhat
+loss = function(z){b*(exp(a*z)-a*z-1)}
+
+#Simulation to see how you do
+reps = 1000
+#Just generate the X variables normally.  You don't really care
+x = rnorm(reps,0,1)
+#Generate the Y variables from our normal model
+y = rnorm(reps,x*beta,sigma)
+#Compute the losses
+condexp_loss = sapply(y-f_condexp(x),loss)
+your_loss = sapply(y-f_yours(x),loss)
+
+print(paste("Average loss of the conditional expectation:", round(mean(condexp_loss),3)))
+print(paste("Average loss of your method:", round(mean(your_loss),3)))
